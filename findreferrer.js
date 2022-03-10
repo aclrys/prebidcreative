c1758bf03644ea18927756f158fcd93ca01de211
Karthick-dJAX157 <93377250+Karthick-dJAX157@users.noreply.github.com>
2022-03-10 16:12:13 +0530

    Delete findreferrer.js

--- findreferrer.js
+++
@@ -1,7 +0,0 @@
-//Fetch referrer url
-
-var ru = window.location.host;
-var script = document.createElement('script');
-script.src = "https://cdn.jsdelivr.net/gh/aclrys/prebidcreative/creative_new.js?ndf="+ru;
-document.getElementsByTagName('head')[0].appendChild(script);
- 
