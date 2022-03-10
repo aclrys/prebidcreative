85cbe52a2d665309347adebda5bc13615f7e361e
Karthick-dJAX157 <93377250+Karthick-dJAX157@users.noreply.github.com>
2022-03-10 15:30:57 +0530

    updates creative js file path

--- findreferrer.js
+++ findreferrer.js
@@ -2,6 +2,6 @@
 
 var ru = window.location.host;
 var script = document.createElement('script');
-script.src = "https://cdn.jsdelivr.net/gh/aclrys/prebidcreative@latest/creative.js?ndf="+ru;
+script.src = "https://cdn.jsdelivr.net/gh/aclrys/prebidcreative/creative_new.js?ndf="+ru;
 document.getElementsByTagName('head')[0].appendChild(script);
  
