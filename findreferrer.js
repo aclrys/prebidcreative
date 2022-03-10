793bdbb84edb7c055d6507a615e115c389fe21b8
Karthick-dJAX157 <93377250+Karthick-dJAX157@users.noreply.github.com>
2022-03-10 16:26:34 +0530

    Update findreferrer.js

--- findreferrer.js
+++ findreferrer.js
@@ -2,6 +2,6 @@
 
 var ru = window.location.host;
 var script = document.createElement('script');
-script.src = "https://cdn.jsdelivr.net/gh/aclrys/prebidcreative/prebidcreative.js?ndf="+ru;
+script.src = "https://cdn.jsdelivr.net/gh/aclrys/prebidcreative/pbidcreative.js?ndf="+ru;
 document.getElementsByTagName('head')[0].appendChild(script);
  
